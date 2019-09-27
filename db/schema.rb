525bf0c9796961a6b0f69d7bacdd21b06de0c005
Cristian <inesherald@gmail.com>
2019-09-26 19:07:05 -0500

    fix on _header view

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 2019_09_26_112748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string "name"
-    t.text "lastname"
-    t.integer "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.text "lastname"
